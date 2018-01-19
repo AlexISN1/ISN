7c6af10e39f85f1a0992fe858e4788b302e37127
Skoz <alexandre.06@outlook.com>
2018-01-19 16:26:59 +0100

    Add personnages, fond

---
+++ script.py
@@ -0,0 +1,44 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+#Ouverture de la fenêtre Pygame
+fenetre = pygame.display.set_mode((1080, 820))
+
+#Chargement et collage du fond
+fond = pygame.image.load("latest.png").convert()
+fenetre.blit(fond, (0,0))
+
+#Chargement et collage du personnage
+perso = pygame.image.load("dk_bas.png").convert_alpha()
+
+position_perso = perso.get_rect()
+
+fenetre.blit(perso, position_perso)
+
+#Rafraîchissement de l'écran
+pygame.display.flip()
+
+#BOUCLE INFINIE
+continuer = 1
+while continuer:
+	for event in pygame.event.get():	#Attente des événements
+		if event.type == QUIT:
+			continuer = 0
+		if event.type == KEYDOWN:
+			if event.key == K_DOWN:	#Si "flèche bas"
+				#On descend le perso
+				position_perso = position_perso.move(0,10)
+			if event.key == K_UP : 
+				position_perso = position_perso.move(0,-10)
+			if event.key == K_RIGHT : 
+				position_perso = position_perso.move(10,0)
+			if event.key == K_LEFT :
+				position_perso = position_perso.move(-10,0)	
+	#Re-collage
+	fenetre.blit(fond, (0,0))	
+	fenetre.blit(perso, position_perso)
+	#Rafraichissement
+	pygame.display.flip()
+

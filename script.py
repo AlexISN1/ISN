79615363573dcdb8cff7026e4ef1fee7ffbb561e
Skoz <alexandre.06@outlook.com>
2018-03-30 11:55:31 +0200

    ajout du temps, niveau, score

--- script.py
+++ script.py
@@ -1,40 +1,34 @@
-#!/usr/bin/python3
-# -*- coding: Utf-8 -*
-
-"""
-Jeu Donkey Kong Labyrinthe
-Jeu dans lequel on doit déplacer DK jusqu'aux bananes à travers un labyrinthe.
-
-Script Python
-Fichiers : dklabyrinthe.py, classes.py, constantes.py, n1, n2 + images
-"""
-
 import pygame
 from pygame.locals import *
-
 from classes import *
 from constantes import *
-
+import time
+from fonction import * 
+from datetime import datetime
+import random
 pygame.init()
 
 pygame.mixer.music.load('son.mp3')
 pygame.mixer.music.play(0)
 
-
 #Ouverture de la fenêtre Pygame (carré : largeur = hauteur)
 fenetre = pygame.display.set_mode((450, 450))
-#Icone
 
- 
 #Titre
 pygame.display.set_caption(titre_fenetre)
 
-
 #BOUCLE PRINCIPALE
 continuer = 1
-while continuer:	
 
+while continuer:
 
+	#score
+	score = {}
+	nom = input('Votre pseudo : ')
+	score[nom] = 1
+	print("Bienvenue dans le jeu du labyrinthe", nom)
+	for keys, i in score.items() : 
+		print("Pseudo : " ,keys + ",score : ", i)
 	#Chargement et affichage de l'écran d'accueil
 	accueil = pygame.image.load(image_accueil).convert()
 	fenetre.blit(accueil, (0,0))
@@ -48,17 +42,16 @@ while continuer:
 
 	#BOUCLE D'ACCUEIL
 	while continuer_accueil:
-	
+
 		#Limitation de vitesse de la boucle
 		pygame.time.Clock().tick(30)
 
 
 		for event in pygame.event.get():
-		
-			#Si l'utilisateur quitte, on met les variables 
+
+			#Si l'utilisateur quitte, on met les variables
 			#de boucle à 0 pour n'en parcourir aucune et fermer
 			if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
-				print("aurevoir")
 
 				continuer_accueil = 0
 				continuer_jeu = 0
@@ -66,54 +59,46 @@ while continuer:
 				continuer = 0
 				#Variable de choix du niveau
 				choix = 0
-				
-			elif event.type == KEYDOWN:				
+
+			elif event.type == KEYDOWN:
 				#Lancement du niveau 1
 				if event.key == K_F1:
 					continuer_accueil = 0	#On quitte l'accueil
-					choix = 'n1'		#On définit le niveau à charger
-				#Lancement du niveau 2
-				elif event.key == K_F2:
-					continuer_accueil = 0
-					choix = 'n2'
+					choix = 'n'+str(random.randint(1,2))		#On définit le niveau à charger
 			
-		
-
 	#on vérifie que le joueur a bien fait un choix de niveau
 	#pour ne pas charger s'il quitte
 	if choix != 0:
 		#Chargement du fond
 		fond = pygame.image.load(image_fond).convert()
-
 		#Génération d'un niveau à partir d'un fichier
 		niveau = Niveau(choix)
 		niveau.generer()
 		niveau.afficher(fenetre)
 
 		#Création de Donkey Kong
-		dk = Perso("images/dk_droite.png", "images/dk_gauche.png", 
-		"images/dk_haut.png", "images/dk_bas.png", niveau)
+		dk = Perso("images/dk_droite.png","images/dk_gauche.png", "images/dk_haut.png", "images/dk_bas.png", niveau)
 
-				
 	#BOUCLE DE JEU
 	while continuer_jeu:
-	
+		maintenant = datetime.now()
+
 		#Limitation de vitesse de la boucle
 		pygame.time.Clock().tick(30)
-	
+
 		for event in pygame.event.get():
-		
+
 			#Si l'utilisateur quitte, on met la variable qui continue le jeu
 			#ET la variable générale à 0 pour fermer la fenêtre
 			if event.type == QUIT:
 				continuer_jeu = 0
 				continuer = 0
-		
+
 			elif event.type == KEYDOWN:
 				#Si l'utilisateur presse Echap ici, on revient seulement au menu
 				if event.key == K_ESCAPE:
 					continuer_jeu = 0
-					
+
 				#Touches de déplacement de Donkey Kong
 				elif event.key == K_RIGHT:
 					dk.deplacer('droite')
@@ -122,8 +107,8 @@ while continuer:
 				elif event.key == K_UP:
 					dk.deplacer('haut')
 				elif event.key == K_DOWN:
-					dk.deplacer('bas')			
-			
+					dk.deplacer('bas')
+
 		#Affichages aux nouvelles positions
 		fenetre.blit(fond, (0,0))
 		niveau.afficher(fenetre)
@@ -132,31 +117,48 @@ while continuer:
 
 		#Victoire -> Retour à l'accueil
 		if niveau.structure[dk.case_y][dk.case_x] == 'a':
-			continuer_accueil = 0
+			maintenant2 = datetime.now()
+
+			if abs(maintenant2.second - maintenant.second) < 10 : 
+
+				score[nom] +=5
+			print(score)
 			continuer_jeu = 0
-			continuer = 0
-			pygame.quit()
-			continuer_jeu_1 = 1
 
-			while continuer_jeu_1 : 
-				a = input()
-				if a == '1' : 
-					continuer = 1
-					continuer_jeu = 1
-					continuer_jeu_1 = 0
+	time.sleep(1)
 
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                     
+	playAgain = "oui"
+	while playAgain == "oui" or playAgain == "o":
+	    displayIntro()
+	    choice = choosePath()
+	    checkPath(choice) 
+	    if choice == 'gauche' : 
+	        if playAgain == 'oui' :
+	            continue
+	    choice = chooseriviere()
+	    checkriviere(choice)
+	    if choice == 'bois' : 
+	        if playAgain == 'oui' :
+	            continue
+	    choice = choosearme()
+	    checkarme(choice)
+	    if choice == 'couteau' or choice == 'epee' : 
+	        if playAgain == 'oui' :
+	            continue
+	    choice = chooseboss()
+	    checkboss(choice)
+	    if choice == 'haut' : 
+	        if playAgain == 'oui' :
+	            continue
+	    choice = choosefin()
+	    checkfin(choice)
+	    if choice == 'coeur' : 
+	        if playAgain == 'oui' :
+	            continue
+	    playAgain = input("Voulez vous rejouer ?(Oui ou o pour rejouer): ")
 
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	continuer_jeu = 1

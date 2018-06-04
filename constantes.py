c7a9b430db8743f14070be254e45f0c98a4cfd04
Skoz <alexandre.06@outlook.com>
2018-06-04 16:53:39 +0200

    Ajout des variables des mini jeux"

--- constantes.py
+++ constantes.py
@@ -7,6 +7,17 @@ from pygame.locals import *
 nombre_sprite_cote = 15
 taille_sprite = 30
 
+PosX=60
+PosY=10
+PosX2=200
+PosY2=480
+dx=5
+dy=2
+score = 0
+scorebis = 1
+nbjeu=0
+
+
 #Personnalisation de la fenêtre
 titre_fenetre = "DK Labyrinthe"
 image_accueil = "images/pageaccueil.png"
